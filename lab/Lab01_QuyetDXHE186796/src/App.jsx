dd405a4e8a91124d05182e258ba1b252ecef7fa6
Quyet Duong Xuan <quyetcoder16@gmail.com>
2026-01-15 11:15:52 +0700

    done lab1

---
+++ lab/Lab01_QuyetDXHE186796/src/App.jsx
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+import Orchids from './components/Orchids';
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Orchids />
+    </>
+  );
+}
+
+export default App;
